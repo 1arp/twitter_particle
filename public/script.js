7bdaa4a9ee60a94d63ddc173db9908fea034ba28
1arp <dekhijaegi@gmail.com>
2019-12-19 19:16:27 +0530

    center circle + text started
    Signed-off-by: 1arp <dekhijaegi@gmail.com>

--- public/script.js
+++ public/script.js
@@ -4,6 +4,8 @@ main_twit.height = window.innerHeight;
 
 
 var main_context = main_twit.getContext('2d')
+var centerRadius = 40;
+
 
 
 function CenterCircle(user,radius){
@@ -12,8 +14,12 @@ function CenterCircle(user,radius){
   this.radius = radius;
 
   this.draw = function(){
+
+    var x = window.innerWidth/2;
+    var y = window.innerHeight/2;
     main_context.beginPath();
-    main_context.arc(window.innerWidth/2,window.innerHeight/2,this.radius,0,Math.PI * 2,false);
+    main_context.arc(x,y,this.radius,0,Math.PI * 2,false);
+    main_context.fillText(this.user,x,y+50)
     main_context.fill();
 
   }
@@ -23,9 +29,11 @@ function CenterCircle(user,radius){
   }
 
 
-
 }
 
+
+var userCircle = new CenterCircle('pranav', centerRadius);
+userCircle.draw();
 
 
 window.addEventListener('resize', ()=>{
