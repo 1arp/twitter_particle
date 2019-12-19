424f04adaf820e3da12d0fe6f2d2fbcc1b0e1fd5
1arp <dekhijaegi@gmail.com>
2019-12-19 21:44:54 +0530

    circle objects
    Signed-off-by: 1arp <dekhijaegi@gmail.com>

--- public/script.js
+++ public/script.js
@@ -4,7 +4,7 @@ main_twit.height = window.innerHeight;
 
 
 var main_context = main_twit.getContext('2d')
-var centerRadius = 40;
+var centerRadius = 30;
 
 
 
@@ -19,8 +19,12 @@ function CenterCircle(user,radius){
     var y = window.innerHeight/2;
     main_context.beginPath();
     main_context.arc(x,y,this.radius,0,Math.PI * 2,false);
-    main_context.fillText(this.user,x,y+50)
     main_context.fill();
+    // text
+    main_context.font = '20px Poppins'
+    main_context.textAlign = 'center'
+    main_context.fillText(this.user,x,y+50)
+    
 
   }
 
@@ -36,7 +40,35 @@ var userCircle = new CenterCircle('pranav', centerRadius);
 userCircle.draw();
 
 
+function FollowingCircle(){
+  
+  this.x = x;
+  this.y = y;
+  this.radius = radius;
+
+
+  this.draw = function(){
+    main_context.beginPath();
+    main_context.arc(this.x,this.y,this.radius,0,Math.PI * 2,false)
+    main_context.fill()
+  }
+
+  this.update = function (){
+    this.draw();
+  }
+
+}
+
+function animate(){
+  requestAnimationFrame(animate);
+  main_context.clearRect(0,0,window.innerWidth,window.innerHeight)
+  userCircle.update()
+}
+
+
 window.addEventListener('resize', ()=>{
   main_twit.width = window.innerWidth;
   main_twit.height = window.innerHeight;
 })
+
+animate();
