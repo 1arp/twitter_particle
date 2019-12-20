65a106680464a126065b7ec3a7bb5dd158ff2e1a
1arp <dekhijaegi@gmail.com>
2019-12-20 16:29:17 +0530

    added twitter funcctionality
    Signed-off-by: 1arp <dekhijaegi@gmail.com>

--- public/script.js
+++ public/script.js
@@ -21,6 +21,17 @@ mouse_hover = {
   y : undefined,
 }
 
+mouse_click = {
+  x : undefined,
+  y : undefined,
+}
+
+window.addEventListener('click',(event)=>{
+  mouse_click.x = event.x;
+  mouse_click.y = event.y;
+  console.log(mouse_click);
+})
+
 window.addEventListener('mousemove',(event)=>{
   mouse_hover.x = event.x;
   mouse_hover.y = event.y;
@@ -57,12 +68,12 @@ function CenterCircle(user,radius){
 
 
 var userCircle = new CenterCircle('pranav', centerRadius);
-userCircle.draw();
+// userCircle.draw();
 
 
 
 
-function FollowingCircle(posrad,posradius,radius,followingUserName){
+function FollowingCircle(posrad,posradius,radius,followingUserName,followingUserHandle){
   this.posrad = posrad;
   var pos = getxyofcircle(posradius,this.posrad);
   this.x = pos.x;
@@ -71,6 +82,7 @@ function FollowingCircle(posrad,posradius,radius,followingUserName){
   this.user = followingUserName;
   this.dposrad =  (Math.random() - 0.5)/100;
   this.startingradius = radius;
+  this.followingUserHandle = followingUserHandle;
 
   this.draw = function(){
     main_context.beginPath();
@@ -103,7 +115,7 @@ function FollowingCircle(posrad,posradius,radius,followingUserName){
     this.y = pos.y;
 
     // on_hover - change size
-    if((mouse_hover.x - this.x) < 20 && (mouse_hover.x - this.x) > -20  ){
+    if((mouse_hover.x - this.x) < 20 && (mouse_hover.x - this.x) > -20 && (mouse_hover.y - this.y)  < 20 && (mouse_hover.y - this.y) > -20){
       if(this.radius < 30)
         this.radius +=1;
     }else if(this.radius > this.startingradius){
@@ -113,6 +125,16 @@ function FollowingCircle(posrad,posradius,radius,followingUserName){
 
     this.draw();
   }
+
+  this.check_click = function(){
+    if((mouse_click.x - this.x) < 20 && (mouse_click.x - this.x) > -20 && (mouse_click.y - this.y ) < 20 && (mouse_click.y - this.y) > -20){
+      mouse_click.x = undefined;
+      mouse_click.y = undefined;
+      init(this.followingUserHandle);
+
+    }
+  }
+
 
 }
 
@@ -143,4 +165,50 @@ window.addEventListener('resize', ()=>{
   main_twit.height = window.innerHeight;
 })
 
-animate();
+var followingArray =[];
+var mainUser;
+
+async function init(twitterhandle){
+
+  mainUser = new CenterCircle(twitterhandle,centerRadius);
+
+  followingArray = [];
+  var followingData = await getFollowing(twitterhandle||'realDonaldTrump');
+  
+  followingData.forEach(element => {
+    var posrad = Math.random() * 2 *Math.PI;
+    var posradius = 100 + Math.random() * ((window.innerHeight/2) - 100);
+    followingArray.push(new FollowingCircle(posrad,posradius,15,element['name'],element['screen_name']))
+  });
+  
+  animate_twitter();
+
+
+
+}
+
+
+function animate_twitter(){
+  requestAnimationFrame(animate_twitter);
+  main_context.clearRect(0,0,window.innerWidth,window.innerHeight)
+
+
+  mainUser.update()
+
+  followingArray.forEach(element => {
+    element.update();
+    element.check_click();
+  })
+
+
+}
+
+// twitter functions
+async function getFollowing(twitterhandle){
+  const response = await fetch('/api/following/' + twitterhandle)
+  const following = await response.json()
+  return following
+}
+
+init('realDonaldTrump');
+// animate();
