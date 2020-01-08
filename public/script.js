d1f47bbbb1732d65956591f631d0a6c05b70712e
1arp <dekhijaegi@gmail.com>
2020-01-08 17:38:13 +0530

    fixed color change on resize
    Signed-off-by: 1arp <dekhijaegi@gmail.com>

--- public/script.js
+++ public/script.js
@@ -54,6 +54,10 @@ function CenterCircle(user,radius){
 
     var x = window.innerWidth/2;
     var y = window.innerHeight/2;
+    main_context.fillStyle = "#ffffff";
+    main_context.strokeStyle = "#ffffff";
+    
+    
     main_context.beginPath();
     main_context.arc(x,y,this.radius,0,Math.PI * 2,false);
     main_context.fill();
@@ -78,6 +82,7 @@ var userCircle = new CenterCircle('pranav', centerRadius);
 
 
 
+// let lastUpd = 0
 
 function FollowingCircle(posrad,posradius,radius,followingUserName,followingUserHandle){
   this.posrad = posrad;
@@ -87,6 +92,7 @@ function FollowingCircle(posrad,posradius,radius,followingUserName,followingUser
   this.radius = radius;
   this.user = followingUserName;
   this.startdposrad =  (Math.random() - 0.5)/100;
+  // this.startdposrad =  0.003;
   this.startingradius = radius;
   this.followingUserHandle = followingUserHandle;
   this.dposrad = this.startdposrad;
@@ -116,15 +122,25 @@ function FollowingCircle(posrad,posradius,radius,followingUserName,followingUser
     
 
   }
-
-  this.update = function (){
+  this.update = function (i){
     //move in circle
     this.posrad += this.dposrad;
     pos = getxyofcircle(posradius,this.posrad);
     this.x = pos.x;
     this.y = pos.y;
 
+
+    // update frequency
+
+    // if(i===5) {
+    //   const now = new Date().getTime()
+    //   console.log(now - lastUpd)
+    //   lastUpd = now
+    // }
     // on_hover - change size
+
+
+
     if((mouse_hover.x - this.x) < 20 && (mouse_hover.x - this.x) > -20 && (mouse_hover.y - this.y)  < 20 && (mouse_hover.y - this.y) > -20){
       if(this.radius < 20){
         this.radius +=1;
@@ -180,6 +196,8 @@ function animate(){
 window.addEventListener('resize', ()=>{
   main_twit.width = window.innerWidth;
   main_twit.height = window.innerHeight;
+  main_context.fillStyle = "#ffffff";
+  main_context.strokeStyle = "#ffffff";
 })
 
 var followingArray =[];
@@ -212,8 +230,8 @@ function animate_twitter(){
 
   mainUser.update()
 
-  followingArray.forEach(element => {
-    element.update();
+  followingArray.forEach((element,i) => {
+    element.update(i);
     element.check_click();
   })
 
